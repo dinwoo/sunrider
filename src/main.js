8ba87f1ba1017d981a5bae596c3edf72c989f680
dinwoo <danielforboy@gmail.com>
2021-10-24 15:29:20 +0800

    chore: 設定svg loader

--- src/main.js
+++ src/main.js
@@ -9,6 +9,14 @@ import "./plugins/axios";
 // mock
 import "./fake-db/index.js";
 
+// svg icon
+const requireAll = (requireContext) =>
+  requireContext.keys().map(requireContext);
+const req = require.context("@/assets/icon", true, /\.svg$/);
+requireAll(req);
+import SvgIcon from "@/components/common/SvgIcon";
+Vue.component("SvgIcon", SvgIcon);
+
 new Vue({
   router,
   store,

baf66d92d46d4166b2265bc4aeb2f6fae18811f8
dinwoo <danielforboy@gmail.com>
2021-10-24 17:49:20 +0800

    chore: deploy 設定

--- deploy-github.sh
+++ deploy-github.sh
@@ -3,10 +3,6 @@
 # 發生錯誤時執行終止指令
 set -e
 
-git checkout feature/github-deploy
-
-git rebase develop
-
 # 打包編譯
 npm run build
 
@@ -26,7 +22,7 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:dinwoo/light-forever.git master:gh-pages
+git push -f git@github.com:dinwoo/sunrider.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
 # git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
 

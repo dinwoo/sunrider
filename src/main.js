e5938f518b2e45cc387114d1fdf9d4a9e9d9cfc7
dinwoo <danielforboy@gmail.com>
2021-10-24 17:08:31 +0800

    Revert "chore: 設定svg loader"
    This reverts commit 8ba87f1ba1017d981a5bae596c3edf72c989f680.

--- src/main.js
+++ src/main.js
@@ -9,14 +9,6 @@ import "./plugins/axios";
 // mock
 import "./fake-db/index.js";
 
-// svg icon
-const requireAll = (requireContext) =>
-  requireContext.keys().map(requireContext);
-const req = require.context("@/assets/icon", true, /\.svg$/);
-requireAll(req);
-import SvgIcon from "@/components/common/SvgIcon";
-Vue.component("SvgIcon", SvgIcon);
-
 new Vue({
   router,
   store,
